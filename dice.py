dbd6e6d6d70ddf355130545fb2829b6b38e62e08
Prachi Bhatt <104725245+PrachiBhatt443@users.noreply.github.com>
2023-01-09 19:57:27 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,79 @@
+import random
+n=int(input("Enter how many dices you want to roll?(1 to 6)"))
+l=[]
+for i in range(n):
+    s=random.randint(1,6)
+    l.append(s)
+print(l)
+
+def generate_dice_faces_diagram(l):
+    DICE_ART = {
+        1: (
+            "┌─────────┐",
+            "│         │",
+            "│    ●    │",
+            "│         │",
+            "└─────────┘",
+        ),
+        2: (
+            "┌─────────┐",
+            "│  ●      │",
+            "│         │",
+            "│      ●  │",
+            "└─────────┘",
+        ),
+        3: (
+            "┌─────────┐",
+            "│  ●      │",
+            "│    ●    │",
+            "│      ●  │",
+            "└─────────┘",
+        ),
+        4: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│         │",
+            "│  ●   ●  │",
+            "└─────────┘",
+        ),
+        5: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│    ●    │",
+            "│  ●   ●  │",
+            "└─────────┘",
+        ),
+        6: (
+            "┌─────────┐",
+            "│  ●   ●  │",
+            "│  ●   ●  │",
+            "│  ●   ●  │",
+            "└─────────┘",
+        ),
+    }
+    DIE_HEIGHT = len(DICE_ART[1])
+    DIE_WIDTH = len(DICE_ART[1][0])
+    DIE_FACE_SEPARATOR = " "
+    # Generate a list of dice faces from DICE_ART
+    dice_faces = []
+    for value in l:
+        dice_faces.append(DICE_ART[value])
+
+# Generate a list containing the dice faces rows
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+
+    # Generate header with the word "RESULTS" centered
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
+y=generate_dice_faces_diagram(l)
+print(y)
